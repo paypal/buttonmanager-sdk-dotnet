57f64e6acc889f8a35917f14b5dc5febce4c4022
unknown <rsomaskandan@D-MAA-00391891.corp.ebay.com>
2012-12-21 21:06:20 +0530

    Updated folder-structure for NuGet

--- ButtonManager.bat
+++ ButtonManager.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_ButtonManager_SDK.sln /build Release %1
+call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_ButtonManager_SDK\PayPal_ButtonManager_SDK.sln /build Release %1
 
 copy /Y PayPal_ButtonManager_SDK\bin\Release\PayPal_ButtonManager_SDK.dll Samples\ButtonManagerAPISample\lib\. 

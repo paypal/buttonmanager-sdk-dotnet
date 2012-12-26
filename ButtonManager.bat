b58459cd7fcdc840ad59c476c0e004b8641db8c0
unknown <rsomaskandan@D-MAA-00391891.corp.ebay.com>
2012-12-27 00:37:18 +0530

    Removed underscore from folders as part of .NET Standards

--- ButtonManager.bat
+++ ButtonManager.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_ButtonManager_SDK\PayPal_ButtonManager_SDK.sln /build Release %1
+call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPalButtonManagerSDK\PayPalButtonManagerSDK.sln /build Release %1
 
-copy /Y PayPal_ButtonManager_SDK\bin\Release\PayPalButtonManagerSDK.dll Samples\ButtonManagerAPISample\lib\. 
+copy /Y PayPalButtonManagerSDK\bin\Release\PayPalButtonManagerSDK.dll Samples\ButtonManagerAPISample\lib\. 

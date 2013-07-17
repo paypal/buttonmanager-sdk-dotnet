5c46d451cc5530da00e7747e78f0965601ea13c8
Rajani <rajanipro@gmail.com>
2013-07-17 15:46:01 +0530

    Updated IPN Config

--- Samples/ButtonManagerAPISample/Configuration.cs
+++ Samples/ButtonManagerAPISample/Configuration.cs
@@ -17,6 +17,9 @@ namespace ButtonManagerAPISample
             configMap.Add("account1.apiPassword", "WX4WTU3S8MY44S7F");
             configMap.Add("account1.apiSignature", "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy");
 
+	    // Optional
+            // configMap.Add("account1.Subject", "");
+
             // Sandbox Email Address
             configMap.Add("sandbox.EmailAddress", "pp.devtools@gmail.com");
 
@@ -37,8 +40,27 @@ namespace ButtonManagerAPISample
             configMap.Add("account2.apiCertificate", "resource/sdk-cert.p12");
             configMap.Add("account2.privateKeyPassword", "password");
 
+	    // Optional
+            // configMap.Add("account1.Subject", "");
+
             // Sandbox Email Address
             configMap.Add("sandbox.EmailAddress", "pp.devtools@gmail.com");
+
+
+            return configMap;
+        }
+
+        // Creates a configuration map containing IPN Endpoint mode and other optional parameters
+        public static Dictionary<string, string> GetConfig()
+        {
+            Dictionary<string, string> configMap = new Dictionary<string, string>();
+
+            // Endpoints are varied depending on whether sandbox OR live is chosen for mode
+            configMap.Add("mode", "sandbox");
+
+            // These values are defaulted in SDK. If you want to override default values, uncomment it and add your value.
+            //configMap.Add("connectionTimeout", "36000");
+            //configMap.Add("requestRetries", "3");
 
             return configMap;
         }
